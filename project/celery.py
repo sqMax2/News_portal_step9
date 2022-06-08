a8a66c614bd06933f6c21556edc872991f96962c
sqMax2 <msvp@mail.ru>
2022-06-09 02:24:17 +0300

    Schedule wasn't tested

--- project/celery.py
+++ project/celery.py
@@ -2,10 +2,10 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-# Event mode runs with gevent:
+# Event mode runs with gevent on Win:
 # celery -A project worker -l info -P gevent
 
-# Celery beat run (doesn't work):
+# Celery beat run on Win (but does nothing):
 # python.exe -m celery -A project beat --loglevel=info
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
@@ -19,8 +19,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'action_every_monday_8am': {
         'task': 'newsapp.tasks.mail',
-        'schedule': crontab(minute='*/1'),
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
         # 'args': (),
     },
 }
-#'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
